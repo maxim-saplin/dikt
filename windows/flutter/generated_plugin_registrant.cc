ef9749700801463b90a93160d266ac4fd05d7ac6
Maxim Saplin <smaxmail@gmail.com>
2021-03-04 19:27:49 +0300

    Dependencies update

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -4,9 +4,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_chooser/file_chooser_plugin.h>
+#include <file_selector_windows/file_selector_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileChooserPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileChooserPlugin"));
+  FileSelectorPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorPlugin"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,7 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  file_chooser
+  file_selector_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
