7f31d0aa159a067a3971c9941d7a528585b56b0f
Maxim Saplin <smaxmail@gmail.com>
2021-11-26 05:31:25 -0800

    # of isolates spawned is cores-2

--- lib/common/isolate_pool.dart
+++ lib/common/isolate_pool.dart
@@ -6,8 +6,10 @@ import 'package:ikvpack/ikvpack.dart';
 late IsolatePool pool;
 
 void initIsolatePool() {
-  //ODO - return pool back to normal
-  pool = IsolatePool(max(Platform.numberOfProcessors - 1, 2));
-  //pool = IsolatePool(2);
+  // ODO - return pool back to normal
+  // There's a bug when there're many isolates created and activity is hidden, resuming to the activity shows it frozen
+  // https://github.com/dart-lang/sdk/issues/47672
+  pool = IsolatePool(max(Platform.numberOfProcessors - 2, 2));
+  //pool = IsolatePool(10);
   pool.start();
 }
