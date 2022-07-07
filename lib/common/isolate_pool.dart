d9b9e48797d2aa403bd137a56945f8c1fb0ebd1b
Maxim Saplin <smaxmail@gmail.com>
2022-07-07 15:04:24 +0300

    +17, more isolates

--- lib/common/isolate_pool.dart
+++ lib/common/isolate_pool.dart
@@ -8,7 +8,7 @@ IsolatePool? pool;
 void initIsolatePool() {
   // There's a bug when there're many isolates created and activity is hidden, resuming to the activity shows it frozen
   // https://github.com/dart-lang/sdk/issues/47672
-  pool = IsolatePool(max(Platform.numberOfProcessors - 1,
+  pool = IsolatePool(max(Platform.numberOfProcessors,
       2)); // more cores lead to faster dictionary loads yet slower lookups
   // ODO - return pool back to normal
   //pool = IsolatePool(2);
